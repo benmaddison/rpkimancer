fb2f5b483c6919a26793a6e9ab5f0e194f77f7c0
Ben Maddison <benm@workonline.africa>
2021-03-30 00:23:01 +0200

    more cert library refactoring

--- rpkimancer/demo.py
+++ rpkimancer/demo.py
@@ -5,13 +5,13 @@ import os
 from cryptography.hazmat.primitives import serialization
 
 from .econtent import RpkiSignedURIList
-from .cert import make_cert
+from .cert import ResourceCertificate
 
 DEMO_ASN = 37271
 DEMO_URI = "https://as37271.fyi/static/net_info_portal/md/bgp-communities.md"
 COMMUNITY_DEFS_OID = (1, 3, 6, 1, 4, 1, 35743, 3)
-DEMO_PUBLICATION_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)),
-                                     "target", "demo", "rpki")
+DEMO_PUB_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)),
+                             "target", "demo", "rpki")
 
 
 def demo():
@@ -29,26 +29,26 @@ def demo():
                             resources=dict(asID=[("id", args.asn)]),
                             type=args.content_type)
     # create TA
-    ta_cert = make_cert(ip_resources=[ipaddress.ip_network("0.0.0.0/0"),
-                                      ipaddress.ip_network("::/0")],
-                        as_resources=[(0, 4294967295)])
-    os.makedirs(os.path.join(DEMO_PUBLICATION_PATH, "TA"), exist_ok=True)
-    with open(os.path.join(DEMO_PUBLICATION_PATH, "TA.cer"), "wb") as f:
-        f.write(ta_cert.public_bytes(serialization.Encoding.DER))
+    ta = ResourceCertificate(ip_resources=[ipaddress.ip_network("0.0.0.0/0"),
+                                           ipaddress.ip_network("::/0")],
+                             as_resources=[(0, 4294967295)])
+    os.makedirs(os.path.join(DEMO_PUB_PATH, "TA"), exist_ok=True)
+    with open(os.path.join(DEMO_PUB_PATH, "TA.cer"), "wb") as f:
+        f.write(ta.cert.public_bytes(serialization.Encoding.DER))
     # create CA2
-    ca_cert_parent = make_cert(common_name="CA1", issuer=ta_cert,
-                               ip_resources=[ipaddress.ip_network("41.78.188.0/22"),
-                                             ipaddress.ip_network("197.157.64.0/19")],
-                               as_resources=[37271])
-    os.makedirs(os.path.join(DEMO_PUBLICATION_PATH, "CA1"), exist_ok=True)
-    with open(os.path.join(DEMO_PUBLICATION_PATH, "TA", "CA1.cer"), "wb") as f:
-        f.write(ca_cert_parent.public_bytes(serialization.Encoding.DER))
-    # create CA2
-    ca_cert_child = make_cert(common_name="CA2", issuer=ca_cert_parent,
+    ca1 = ResourceCertificate(common_name="CA1", issuer=ta,
+                              ip_resources=[ipaddress.ip_network("41.78.188.0/22"),
+                                            ipaddress.ip_network("197.157.64.0/19")],
                               as_resources=[37271])
-    os.makedirs(os.path.join(DEMO_PUBLICATION_PATH, "CA2"), exist_ok=True)
-    with open(os.path.join(DEMO_PUBLICATION_PATH, "TA", "CA2.cer"), "wb") as f:
-        f.write(ca_cert_child.public_bytes(serialization.Encoding.DER))
+    os.makedirs(os.path.join(DEMO_PUB_PATH, "CA1"), exist_ok=True)
+    with open(os.path.join(DEMO_PUB_PATH, "TA", "CA1.cer"), "wb") as f:
+        f.write(ca1.cert.public_bytes(serialization.Encoding.DER))
+    # create CA2
+    ca2 = ResourceCertificate(common_name="CA2", issuer=ca1,
+                              as_resources=[37271])
+    os.makedirs(os.path.join(DEMO_PUB_PATH, "CA2"), exist_ok=True)
+    with open(os.path.join(DEMO_PUB_PATH, "TA", "CA2.cer"), "wb") as f:
+        f.write(ca2.cert.public_bytes(serialization.Encoding.DER))
     # create RSU
     print(rsu.to_asn1())
 
