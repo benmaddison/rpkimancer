9e1d4c40fd590dd893eb0c3db53966057b905ff4
Ben Maddison <benm@workonline.africa>
2021-04-10 13:13:24 +0200

    add license

--- rpkimancer/sigobj/gbr.py
+++ rpkimancer/sigobj/gbr.py
@@ -1,3 +1,14 @@
+# Copyright (c) 2021 Ben Maddison. All rights reserved.
+#
+# The contents of this file are licensed under the MIT License
+# (the "License"); you may not use this file except in compliance with the
+# License.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
 from .base import EncapsulatedContent, SignedObject
 from ..asn1 import RPKIGhostbusters
 from ..resources import INHERIT_AS, INHERIT_IPV4, INHERIT_IPV6
