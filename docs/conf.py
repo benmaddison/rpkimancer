f334648ee753082bd02ee0258ba4708ba9148128
Ben Maddison <benm@workonline.africa>
2021-05-04 09:48:00 +0200

    clean up docs config

--- docs/conf.py
+++ docs/conf.py
@@ -1,95 +1,89 @@
-# Configuration file for the Sphinx documentation builder.
+# Copyright (c) 2021 Ben Maddison. All rights reserved.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
+# The contents of this file are licensed under the MIT License
+# (the "License"); you may not use this file except in compliance with the
+# License.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+"""rpkimancer documentation config."""
+
+from __future__ import annotations
+
+import datetime
 import importlib.metadata
 
 import sphinx_readable_theme
 
-# -- Project information -----------------------------------------------------
+import rpkimancer
 
-project = 'rpkimancer'
-copyright = '2021, Ben Maddison'
-author = 'Ben Maddison'
+_dist = importlib.metadata.distribution(rpkimancer.__name__)
+_buildtime = datetime.datetime.utcnow()
 
-# The full version, including alpha/beta/rc tags
-release = importlib.metadata.version(project)
+# -- Project Information
+
+project = _dist.metadata["name"]
+author = _dist.metadata["author"]
+
+_from_year = 2021
+_to_year = _buildtime.year
+if _from_year < _to_year:
+    _year_range = f"{_from_year}-{_to_year}"
+else:
+    _year_range = f"{_from_year}"
+copyright = f"{_year_range}, {author}"
+
+release = _dist.version
+version = ".".join(release.split(".")[:2])
 
 
-# -- General configuration ---------------------------------------------------
+# -- General configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinx.ext.autodoc",
     "myst_parser",
     "sphinx_autodoc_typehints",
     "sphinx_multiversion",
 ]
-
-# Add any paths that contain templates here, relative to this directory.
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+exclude_patterns = []
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+# -- HTML output
 
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-# html_theme = 'alabaster'
-html_theme_path = [sphinx_readable_theme.get_html_theme_path()]
 html_theme = 'readable'
-
+html_theme_path = [sphinx_readable_theme.get_html_theme_path()]
+html_static_path = ['_static']
 html_sidebars = {
     "**": [
         "localtoc.html",
         "relations.html",
-        "sourcelink.html",
         "versions.html",
         "searchbox.html",
     ],
 }
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# -- Autodoc configuration
 
-# html_theme_options = {
-#         "github_user": "benmaddison",
-#         "github_repo": "rpkimancer",
-#         "github_button": True,
-#         "github_banner": True,
-# }
+autodoc_member_order = "bysource"
 
-# mixed source parsing with myst
-source_suffix = {
-    ".rst": "restructuredtext",
-    ".md": "markdown",
-}
+# -- Markdown processing
+
 myst_enable_extensions = [
     "colon_fence",
 ]
 
-# set_type_checking_flag = True
+# -- Typehint processing
+
 always_document_param_types = True
+
+# -- Multiversion processing
 
 smv_prebuild_command = "sphinx-apidoc --separate " \
                                      "--force " \
