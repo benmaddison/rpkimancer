d3481085348e578fd3242b25e482a2cb100df126
benmaddison <benm@workonline.africa>
2021-11-09 19:00:23 +0000

    Deploying to gh-pages from @ benmaddison/rpkimancer@124f8425b71c85c631f54f814fcff8c6458e665a 🚀

--- dependency-updates/_static/documentation_options.js
+++ dependency-updates/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.2.dev47',
+    VERSION: '0.1.2.dev49',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
